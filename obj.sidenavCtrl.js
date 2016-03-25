99213aea4d58b48be7f902abea32dc02cd624828
Felix Tram <felix@felixmac.local>
2016-03-25 11:55:57 -0600

    formatting

--- obj.sidenavCtrl.js
+++ obj.sidenavCtrl.js
@@ -5,6 +5,7 @@
   function sidenavCtrl() {
     var vm = this;
     vm.hello = "string";   
-
+    //need vm.someobject to hold what is passed from other controller
+    //need to inject service into controller
   };
 })();
